b9d8e4492dd88cd755d6fc49582b003b895a302b
orzero <xami520@gmail.com>
2011-06-27 12:44:24 +0800

    tool

---
+++ protected/components/Xuk.php
@@ -0,0 +1,21 @@
+<?php
+class Xuk
+{
+	//取得item地址列表,入库
+	public function getList()
+	{
+		$id=$_R
+	}
+	
+	//取得item具体内容
+	public function getItem()
+	{
+		
+	}
+	
+	//处理图片,生成缩略图,更新图片入库状态
+	public function getImage()
+	{
+		
+	}
+}
--- protected/controllers/ToolController.php
+++ protected/controllers/ToolController.php
@@ -115,68 +115,17 @@ class ToolController extends Controller
 		
 		$this->render('in2out', $data);
 	}
+	
 	public function actionGet()
 	{
 		$show = isset($_REQUEST['show']) ? true : false;
 		$expire = isset($_REQUEST['expire']) ? intval($_REQUEST['expire']) : 10;
 		$src = isset($_REQUEST['src']) ? trim($_REQUEST['src']) : '';
 		
-		$o = $this->OZCurl($src, $expire, $show);
+		$o = Tools::OZCurl($src, $expire, $show);
 	}
 	
 	
-	public function OZCurl($src, $expire=60, $show=false)
-	{
-//		$show = isset($_REQUEST['show']) ? intval($_REQUEST['show']) : false;
-//		$expire = (isset($_REQUEST['expire']) && (intval($_REQUEST['expire'])>10)) ? intval($_REQUEST['expire']) : 10;
-//		$src = isset($_REQUEST['src']) ? trim($_REQUEST['src']) : '';
-//		if(empty($src)){
-//			return false;
-//		}
-		
-		$expire = intval($expire)>10 ? intval($expire) : 10;
-		$src = trim($src);
-		if(empty($src)) return false;
-		
-		$c = null;
-		$key = md5($src);
-		$cache = Yii::app()->cache;
-		$c=$cache->get($key);
-		
-		if(empty($c)){
-			//Run curl
-			$curl = Yii::app()->CURL;
-			$curl->run(array(CURLOPT_REFERER => $src));
-			$curl->setUrl($src);
-			$curl->exec();
-			
-			if(Yii::app()->CURL->isError()) {
-				// Error
-				var_dump($curl->getErrNo());
-				var_dump($curl->getError());
-				
-			}else{
-				// More info about the transfer
-				$c=array(
-					'ErrNo'=>$curl->getErrNo(),
-					'Error'=>$curl->getError(),
-					'Header'=>$curl->getHeader(),
-					'Info'=>$curl->getInfo(),
-					'Result'=>$curl->getResult(),
-				);
-			}
-			$cache->set($key, $c, $expire);
-		}
-		
-		if($show==true){
-			if(!empty($c['Info']['content_type']))
-				header('Content-Type: '.$c['Info']['content_type']);
-			if($c['Info']['http_code']==200)
-				echo $c['Result'];
-		}else{
-			return $c;
-		}
-	}
 
 	// Uncomment the following methods and override them if needed
 	/*
